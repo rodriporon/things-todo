fc52c976cecd8bd389f364be54b4b40808a668f6
rodriporon <3065713820401@ingenieria.usac.edu.gt>
2021-12-02 13:37:31 -0600

    checkbox creado

---
+++ src/screens/main.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback, useState } from 'react'
+import { Pressable } from 'react-native'
+import {
+    Text,
+    Box,
+    Center,
+    VStack, useColorModeValue
+} from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox'
+
+export default function MainScreen() {
+    const [checked, setChecked] = useState(false)
+    const handlePressCheckbox = useCallback(() => {
+        setChecked(prev => !prev)
+    }, [])
+
+    return (
+        <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
+            <VStack space={5} alignItems="center">
+                <Box w="100px" h="100px">
+                    <Pressable onPress={handlePressCheckbox}>
+                        <AnimatedCheckbox checked={checked} />
+                    </Pressable>
+                    </Box>
+            <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+                <Text>Hello</Text>
+            </Box>
+            <ThemeToggle />
+            </VStack>
+        </Center>
+    )
+}
